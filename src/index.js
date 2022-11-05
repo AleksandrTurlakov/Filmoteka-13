3428f4f2e8e718367bcecd29d06958c18a7a6b92
AleksandrTurlakov <aturlakov@ukr.net>
2022-11-06 00:43:43 +0200

    add scrollBtn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { getFilms } from './js/getFilm';
 // import markup from './js/templates/markup.hbs';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { all } from "./js/first_page.js";
+import { all } from './js/first_page.js';
+import { scrollBtn } from './js/scrollBtn';
