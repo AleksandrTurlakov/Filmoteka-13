4620ac3d27541e817fd186d7ef27aa719dd39220
AleksandrTurlakov <aturlakov@ukr.net>
2022-11-06 23:41:37 +0200

    fixed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import { all } from './js/first_page';
 import { modal } from './js/modal';
-import { loader } from './js/loader';
 import { scrollBtn } from './js/scrollBtn';
+import { loader } from './js/loader';
