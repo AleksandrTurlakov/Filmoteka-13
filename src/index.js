d650bc5a3e06884f69d025eb5974ac6a0debade9
Alex-Nik-Vel <alex.nik.vel@gmail.com>
2022-11-06 20:19:18 +0200

    fix search film h 20-20 day 06-11-2022

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import { getFilms } from './js/getFilm';
+// import { getFilms } from './js/getFilm';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { all } from './js/first_page.js';
 import { loader } from './js/loader';
 import { scrollBtn } from './js/scrollBtn';
 import { all } from './js/searchFilms';
+import { all } from './js/getFilmsApi';
