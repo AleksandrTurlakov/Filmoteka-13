6ef0e9c9c5814a4a07e5c2164ed483fd6b4720e0
AleksandrTurlakov <aturlakov@ukr.net>
2022-11-06 03:01:29 +0200

    add loader

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { getFilms } from './js/getFilm';
-// import markup from './js/templates/markup.hbs';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { all } from './js/first_page.js';
+import { loader } from './js/loader';
 import { scrollBtn } from './js/scrollBtn';
