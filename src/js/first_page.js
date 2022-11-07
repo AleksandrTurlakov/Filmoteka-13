2f1af13473422abdba2fb280b5378aca2e1bc24a
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2022-11-07 18:38:55 +0200

    Update

--- src/js/first_page.js
+++ src/js/first_page.js
@@ -1,9 +1,15 @@
 import { getDataApi } from './getDataApi';
 import card from './templates/card.hbs';
-
+const filter = document.querySelector('.filter');
 const container = document.querySelector('.container');
 const filmList = document.querySelector('.film-list');
+let URL = '';
 let page = 1;
+const URL_TO_WEEK = `https://api.themoviedb.org/3/trending/movie/week?api_key=7bfeb33324f72574136d1cd14ae769b5&page=`;
+const URL_TO_DAY = `https://api.themoviedb.org/3/trending/movie/day?api_key=7bfeb33324f72574136d1cd14ae769b5&page=`;
+const URL_TO_TOP=`https://api.themoviedb.org/3/movie/top_rated?api_key=7bfeb33324f72574136d1cd14ae769b5&language=en-US&page=`
+const URL_TO_NEW= `https://api.themoviedb.org/3/movie/now_playing?api_key=7bfeb33324f72574136d1cd14ae769b5&language=en-US&page=`
+ URL = URL_TO_WEEK;
 const genres = {
   28: 'Action',
   12: 'Adventure',
@@ -34,7 +40,65 @@ const genres = {
 //     .then(res => res.results);
 // }
 
-function buildElements(response) {
+
+function onButtonChange(event){
+  page = 1;
+  switch (event.target.value) {
+
+    case "top_for_week":
+      filmList.innerHTML = '';
+      URL = URL_TO_WEEK;
+      mainPage(URL, page);
+    break;
+
+    case "top_for_day":
+      filmList.innerHTML = '';
+      URL = URL_TO_DAY;
+      mainPage(URL, page);
+    break;
+
+    case "top_rated":
+      filmList.innerHTML = '';
+      URL = URL_TO_TOP 
+      mainPage(URL, page);
+      break;
+    
+    case "new_films":
+      filmList.innerHTML = '';
+      URL = URL_TO_NEW 
+      mainPage(URL, page);
+      break;
+}
+}
+
+filter.addEventListener('change', onButtonChange)
+mainPage(URL, page);
+
+function mainPage(URL,page) {
+getDataApi(
+  URL+page
+).then(response => buildElements(response));
+
+}
+function addPage() {
+  page += 1;
+  getDataApi(
+   URL+page
+  ).then(res => buildElements(res));
+  if (page === 1000) {
+    window.removeEventListener('scroll', createElafterScroll);
+  }
+}
+
+function createElafterScroll() {
+  if (
+    window.scrollY + window.innerHeight >=
+    document.documentElement.scrollHeight
+  ) {
+    addPage();
+  }
+}
+  function buildElements(response) {
   response.map(item => {
     function auditGanres() {
       if (item.genre_ids.length < 3) {
@@ -53,37 +117,14 @@ function buildElements(response) {
       }
       return `https://image.tmdb.org/t/p/w500${item.poster_path}`;
     }
+    const id = item.id;
     const genr = auditGanres();
     const vote = item.vote_average.toFixed(1);
     const name = item.title.toUpperCase();
     const year = item.release_date.slice(0, 4);
     const src = srcAudit();
-    const data = { name, year, genr, vote, src };
+    const data = { name, year, genr, vote, src, id };
     filmList.insertAdjacentHTML('beforeend', card(data));
   });
 }
-
-getDataApi(
-  `https://api.themoviedb.org/3/trending/movie/week?api_key=7bfeb33324f72574136d1cd14ae769b5&page=${page}`
-).then(response => buildElements(response));
-
-function createElafterScroll() {
-  if (
-    window.scrollY + window.innerHeight >=
-    document.documentElement.scrollHeight
-  ) {
-    addPage();
-  }
-}
-
-function addPage() {
-  page += 1;
-  getDataApi(
-    `https://api.themoviedb.org/3/trending/movie/week?api_key=7bfeb33324f72574136d1cd14ae769b5&page=${page}`
-  ).then(res => buildElements(res));
-  if (page === 1000) {
-    window.removeEventListener('scroll', createElafterScroll);
-  }
-}
-
 window.addEventListener('scroll', createElafterScroll);
