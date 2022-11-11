dc549677c1260c88acb80917f0a154d9c02b8c99
AlekseyGaman <4117988@gmail.com>
2022-11-11 18:11:11 +0200

    change №2

--- src/js/getWatchedQueue.js
+++ src/js/getWatchedQueue.js
@@ -1,48 +1,20 @@
-// import { buildElements } from './first_page.js';
-
-const library = document.querySelector('.library__list');
-const watchedBtn = document.querySelector('.watched');
-const queueBtn = document.querySelector('.queue');
-
-const libraryWatchedBtn = document.getElementById('watched-btn');
-const libraryQueueBtn = document.getElementById('queue-btn');
-
-const watched = localStorage.getItem((key = 'watched'));
-const queue = localStorage.getItem((key = 'queue'));
-// console.log(watched);
-// console.log(queue);
-
-function getWatched() {
-  if (watched !== null) {
-    return JSON.parse(watched);
-  }
-  return [];
+import cardEl from './js/templates/card.hbs';
+const libraryUl = document.querySelector('.library__list');
+const watchedBtn = document.querySelector('#watched-btn');
+const queueBtn = document.querySelector('#queue-btn');
+watchedBtn.addEventListener('click', onWatchedClick);
+queueBtn.addEventListener('click', onQueueClick);
+function onWatchedClick() {
+  libraryUl.innerHTML = '';
+  libraryUl.insertAdjacentHTML(
+    'beforeend',
+    JSON.parse(localStorage.getItem('watched')).map(cardEl).join('')
+  );
 }
-function getQueue() {
-  if (queue !== null) {
-    return JSON.parse(queue);
-  }
-  return [];
-}
-getQueue();
-getWatched();
-console.log(getWatched(), getQueue());
-
-function storageHtml() {
-  watched.forEach(({ name, year, genr, vote, src, id }) => {
-    // let activeBtn = '';
-
-    htmlLibrary += `<li class='film-item' data-id=${id}>
-    <img class='main-image' loading='lazy' src='${src}' alt='${name}' />
-    <div class='tex-item'>
-      <p class='item-name'>${name}</p>
-      <div class='info'>
-        <p class='item-genre'>${genr}</p>
-        <p class='item-year'>${year}<span class='vote'>${vote}</span></p>
-      </div>
-    </div>
-  </li>`;
-    library.classList.remove('library');
-    library.insertAdjacentHTML('beforeend', htmlLibrary);
-  });
+function onQueueClick() {
+  libraryUl.innerHTML = '';
+  libraryUl.insertAdjacentHTML(
+    'beforeend',
+    JSON.parse(localStorage.getItem('queue')).map(cardEl).join('')
+  );
 }
