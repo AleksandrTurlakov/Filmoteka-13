abe535bbefd9fab875fc517ec8ad7f3fc47631a5
AlekseyGaman <4117988@gmail.com>
2022-11-11 16:36:14 +0200

    change cod

--- src/js/getWatchedQueue.js
+++ src/js/getWatchedQueue.js
@@ -1,67 +1,48 @@
 // import { buildElements } from './first_page.js';
-// import myLibraryPage from './myLibraryPage.js';
-// import { handleClick } from './addWatchedQue.js';
 
 const library = document.querySelector('.library__list');
-// const watchedBtn = document.getElementById('watched-btn');
-// const queueBtn = document.getElementById('queue-btn');
+const watchedBtn = document.querySelector('.watched');
+const queueBtn = document.querySelector('.queue');
 
-// const localStorageWatched = Watched => {};
-// console.log(localStorage.getItem((key = 'watched')));
+const libraryWatchedBtn = document.getElementById('watched-btn');
+const libraryQueueBtn = document.getElementById('queue-btn');
 
-// function getWatched(key) {
-//   const LocalWatched = localStorage.getItem((key = 'watched'));
-//   // const watched = JSON.parse(LocalWatched);
-//   console.log(LocalWatched);
-// }
+const watched = localStorage.getItem((key = 'watched'));
+const queue = localStorage.getItem((key = 'queue'));
+// console.log(watched);
+// console.log(queue);
 
-// function getQueue(key) {
-//   const queue = localStorage.getItem((key = 'queue'));
-//   console.log(queue);
-// }
+function getWatched() {
+  if (watched !== null) {
+    return JSON.parse(watched);
+  }
+  return [];
+}
+function getQueue() {
+  if (queue !== null) {
+    return JSON.parse(queue);
+  }
+  return [];
+}
+getQueue();
+getWatched();
+console.log(getWatched(), getQueue());
 
-let watchedList = JSON.parse(localStorage.getItem((key = 'watched')));
-console.log(watchedList);
+function storageHtml() {
+  watched.forEach(({ name, year, genr, vote, src, id }) => {
+    // let activeBtn = '';
 
-// window.addEventListener('storage', event => {
-//   console.log(event);
-// });
-
-function buildLibrary(libs) {
-  allResults = libs.total_results;
-
-  libs.results.map(item => {
-    function auditGanres() {
-      if (item.genre_ids.length < 3) {
-        return item.genre_ids.map(elem => genres[elem]).join(', ');
-      }
-      return (
-        item.genre_ids
-          .map(elem => genres[elem])
-          .slice(0, 2)
-          .join(', ') + ', others'
-      );
-    }
-    function auditYear() {
-      if (!item.release_date) {
-        return 'unknown year';
-      } else return item.release_date.slice(0, 4);
-    }
-    function srcAudit() {
-      if (!item.poster_path) {
-        return `https://st4.depositphotos.com/14953852/22772/v/600/depositphotos_227725020-stock-illustration-no-image-available-icon-flat.jpg`;
-      }
-      return `https://image.tmdb.org/t/p/w500${item.poster_path}`;
-    }
-    const genr = auditGanres();
-    const vote = item.vote_average.toFixed(1);
-    const name = item.title.toUpperCase();
-    const year = auditYear();
-    const src = srcAudit();
-    const id = item.id;
-
-    const data = { name, year, genr, vote, src, id };
-
-    library.insertAdjacentHTML('beforeend', card(data));
+    htmlLibrary += `<li class='film-item' data-id=${id}>
+    <img class='main-image' loading='lazy' src='${src}' alt='${name}' />
+    <div class='tex-item'>
+      <p class='item-name'>${name}</p>
+      <div class='info'>
+        <p class='item-genre'>${genr}</p>
+        <p class='item-year'>${year}<span class='vote'>${vote}</span></p>
+      </div>
+    </div>
+  </li>`;
+    library.classList.remove('library');
+    library.insertAdjacentHTML('beforeend', htmlLibrary);
   });
 }
