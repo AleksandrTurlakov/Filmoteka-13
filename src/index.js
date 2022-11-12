09008ab7dd4db90b7fd95012d8d19f87e62118b9
Alex-Nik-Vel <alex.nik.vel@gmail.com>
2022-11-12 20:45:09 +0200

    вернул назад import в index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { all } from './js/searchFilms';
 import { all } from './js/getFilmsApi';
 import { all } from './js/first_page';
-import  './js/modal';
+import { modal } from './js/modal';
 import { scrollBtn } from './js/scrollBtn';
 import { loader } from './js/loader';
 import { addWatchedQue } from './js/addWatchedQue';
 import './js/footer-modal.js';
-import  './js/modal';
+
