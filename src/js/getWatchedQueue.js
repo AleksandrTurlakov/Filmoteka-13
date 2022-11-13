ec8363c3dd1cfe5bf45e83c96aab6fab2d469a46
AlekseyGaman <4117988@gmail.com>
2022-11-13 02:40:11 +0200

    delete buttons - library page background

--- src/js/getWatchedQueue.js
+++ src/js/getWatchedQueue.js
@@ -1,42 +1,35 @@
+// import myLibraryPage from '../js/templates/libraryPlug.hbs';
 
-// import myLibraryPage from './myLibraryPage.js';
-
-import cardEl from '../js/templates/card.hbs';
+import cardEl from '../js/templates/libraryCard.hbs';
 
 const libraryUl = document.querySelector('.library__list');
+const libraryBack = document.querySelector('.library');
+
 const watchedBtn = document.querySelector('#watched-btn');
 const queueBtn = document.querySelector('#queue-btn');
 
-// const markupLibrary = `<div class="library">
-// <div class="library-bg">
-//  <div class="library-bg__image"></div>
-//  <p class="library-bg__message">You have nothing here!</p>
-//  <p class="library-bg__message">Don't forget to add some movie!</p>
-// </div>
-// </div>`;
+const watched = JSON.parse(localStorage.getItem('watched'));
+const queue = JSON.parse(localStorage.getItem('queue'));
 
 watchedBtn.addEventListener('click', onWatchedClick);
 queueBtn.addEventListener('click', onQueueClick);
 
 function onWatchedClick() {
   libraryUl.innerHTML = '';
-  libraryUl.insertAdjacentHTML(
-    'beforeend',
-    JSON.parse(localStorage.getItem('watched')).map(cardEl).join('')
-  );
+  libraryUl.insertAdjacentHTML('beforeend', watched.map(cardEl).join(''));
+  libraryUl.classList.remove('visually-hidden');
+  libraryBack.classList.add('visually-hidden');
   watchedBtn.classList.toggle('activeBtn');
   queueBtn.classList.remove('activeBtn');
-  // if (onWatchedClick().length === null) {
-  //   myLibraryPage();
-  // }
+
+  watched.forEach(index => console.log(index));
 }
 
 function onQueueClick() {
   libraryUl.innerHTML = '';
-  libraryUl.insertAdjacentHTML(
-    'beforeend',
-    JSON.parse(localStorage.getItem('queue')).map(cardEl).join('')
-  );
+  libraryUl.insertAdjacentHTML('beforeend', queue.map(cardEl).join(''));
+  libraryUl.classList.remove('visually-hidden');
+  libraryBack.classList.add('visually-hidden');
   queueBtn.classList.toggle('activeBtn');
   watchedBtn.classList.remove('activeBtn');
 }
--- src/myLibrary.js
+++ src/myLibrary.js
@@ -1,3 +1,2 @@
-import './js/myLibraryPage.js';
 import './js/getWatchedQueue.js';
 import { scrollBtn } from './js/scrollBtn';
